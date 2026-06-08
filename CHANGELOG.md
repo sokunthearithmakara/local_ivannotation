a9c38509a8378bd6d0df9d32cc5f32732de79c5e
sokunthearithmakara <smakara@hawaii.edu>
2026-06-08 22:09:58 +0700

    Release [1.7.1+] - 2026-06-08

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to this project will be documented in this file.
 
+## [1.7.1+] - 2026-06-08
+
+- Refresh Khmer (km) language strings.
+- Require Interactive Video 1.8.1+ (2026060800).
+
 ## [1.0] - 2024-11-05
 
 ### First release
--- version.php
+++ version.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_ivannotation';
 $plugin->release      = '1.7.1';
-$plugin->version      = 2026051500;
+$plugin->version      = 2026060800;
 $plugin->requires     = 2021112800;
 $plugin->supported    = [400, 502];
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'interactivevideo' => 2025011305,
+    'interactivevideo' => 2026060800,
     'ivplugin_richtext' => 2024071500,
 ];
